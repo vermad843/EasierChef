76cc79737e36f8137fba13207be0274d73b2b629
vermad843 <vishnuverma7524@gmail.com>
2021-03-08 02:01:22 +0530

    working array

--- src/component/Form.js
+++ src/component/Form.js
@@ -1,14 +1,48 @@
-import React  from 'react'
-
+import React, {useState,useCallback}  from 'react';
 import './Form.css';
 
-function Signup() {
+
+function Form() {
+    const [name, setName] = useState([{
+        dish : '',
+        cook : '',
+        picture : '',
+        name : '',
+        quantity : '',
+        measurement : ''
+    }]);
+  
+    const [userDetails, setUserDetails] = useState([]);
+
+
+    const handleChange = useCallback((e) => {
+        setName({
+            ...name,
+            [e.target.name] : e.target.value
+        }) 
+    });
+
+    const handleSubmit = useCallback((e) => {
+        e.preventDefault();
+        setUserDetails([
+                ...userDetails,
+                {
+                    dish : name.dish,
+                    cook : name.cook,
+                    picture : name.picture,
+                    name : name.name,
+                    quantity : name.quantity,
+                    measurement : name.measurement
+                }
+        ])
+    });
+
     return (
         <>
         <div className = "wrapper">         
            <div className = "form-wrapper">
                 <h1>Create Account</h1>
-                <form >
+                <form onSubmit = {handleSubmit}>
                    <div className = "dish">
                        <label htmlFor = "dish">Dish</label>
                        <input 
@@ -16,6 +50,8 @@ function Signup() {
                           className = ""
                           placeholder = "Name of the dish"
                           name = "dish"
+                          value = {name.dish}
+                          onChange = {handleChange}
                           />
                    </div>
                    <div className = "cook">
@@ -25,23 +61,30 @@ function Signup() {
                           className = ""
                           placeholder = "Steps to cook"
                           name = "cook"
+                          value = {name.cook}
+                          onChange = {handleChange}
                           />
-                   </div><div className = "picture">
+                   </div>
+                   <div className = "picture">
                        <label htmlFor = "picture">Picture</label>
                        <input 
                           type = "text"
                           className = ""
                           placeholder = "Picture of dish in the form of URL"
                           name = "picture"
+                          value = {name.picture}
+                          onChange = {handleChange}
                           />
                    </div>
                    <div className = "list">
-                       <label htmlFor = "Name">Ingredient Name</label>
+                       <label htmlFor = "name">Ingredient Name</label>
                        <input 
                           type = "text"
                           className = ""
                           placeholder = "Ingredient Name"
-                          name = "Name"
+                          name = "name"
+                          value = {name.name}
+                          onChange = {handleChange}
                           />
                        <label htmlFor = "quantity">Ingredient quantity</label>
                        <input 
@@ -49,6 +92,8 @@ function Signup() {
                           className = ""
                           placeholder = "Ingredient quantity"
                           name = "quantity"
+                          value = {name.quantity}
+                          onChange = {handleChange}
                           />
                        <label htmlFor = "measurement">Unit of measurement for ingredient qty</label>
                         <input 
@@ -56,16 +101,33 @@ function Signup() {
                           className = ""
                           placeholder = "Unit of measurement for ingredient qty"
                           name = "measurement"
+                          value = {name.measurement}
+                          onChange = {handleChange}
                           />
                    </div>
                    <div className = "submit">
                         <button type = "submit">Submit</button>
                    </div>
                 </form>
+                <ul>
+                    {
+                        userDetails.map((user,i) => (
+                            <li key = {i}>
+                              Dish :       <h1>{user.dish}</h1>
+                              Cook :       <h1>{user.cook}</h1>
+                              Picture :    <h1>{user.picture}</h1>
+                              Name :       <h1>{user.name}</h1>
+                              Quantity :   <h1>{user.quantity}</h1>
+                              Measurement: <h1>{user.measurement}</h1>
+                              <br/>
+                            </li>
+                        ))
+                    }
+                </ul>
             </div>
         </div>
         </>
     )
 }
 
-export default Signup;
+export default Form;
